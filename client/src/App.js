af1bf1738e8f84e9e51db86957fe6e7a76541f91
roee zach <roeezach15@gmail.com>
2023-11-09 18:13:30 +0200

    intial commit

--- client/src/App.js
+++ client/src/App.js
@@ -20,7 +20,7 @@ function App() {
             Learn React
           </a>
           <Link to="/">Home</Link>
-          <Link to="/otherpage">Other Page</Link>
+          <Link to="/otherpage">Other Page and version 2</Link>
         </header>
         <div>
           <Route exact path="/" component={Fib} />
